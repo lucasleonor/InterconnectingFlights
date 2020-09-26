c8a1f08696a3a6309958716786db710ecedf8c28
Lucas Leonor <lucas.g.leonor@gmail.com>
2020-09-25 22:20:22 -0300

    Initial commit

---
+++ src/test/java/com/ryanair/task/interconnectingflights/InterconnectingFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanair.task.interconnectingflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterconnectingFlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
