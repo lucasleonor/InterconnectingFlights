51d4a2ee38a34f1429c28d51123739fda1919b03
Lucas Leonor <lucas.g.leonor@gmail.com>
2020-09-27 00:00:57 -0300

    Added test to InterconnectingFlightsService.findFlights

--- src/test/java/com/ryanair/task/interconnectingflights/service/InterconnectingFlightsServiceTest.java
+++ src/test/java/com/ryanair/task/interconnectingflights/service/InterconnectingFlightsServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.doReturn;
 
 @MockitoSettings
 class InterconnectingFlightsServiceTest {
@@ -23,21 +24,46 @@ class InterconnectingFlightsServiceTest {
     @Mock
     private ScheduleIntegrationService scheduleIntegrationService;
     private InterconnectingFlightsService service;
+    private String departure;
+    private String connection;
+    private String arrival;
 
     @BeforeEach
     void setUp() {
         service = new InterconnectingFlightsService(routeIntegrationService, scheduleIntegrationService);
+        departure = RandomString.make();
+        connection = RandomString.make();
+        arrival = RandomString.make();
     }
 
     @Test
     void findFlights() {
+        doReturn(Set.of(connection)).when(routeIntegrationService).getConnectingAirports(departure, arrival);
+        LocalDateTime departureDateTime = randomDateTime();
+        LocalDateTime arrivalDateTime = randomDateTime();
+        Connection directFlight = new Connection(departure, arrival, departureDateTime, arrivalDateTime);
+        doReturn(Set.of(
+                directFlight
+        )).when(scheduleIntegrationService).findAllFlights(departure, arrival, departureDateTime, arrivalDateTime);
+        Connection flight1 = new Connection(departure, connection, departureDateTime, departureDateTime.plusHours(1));
+        doReturn(Set.of(
+                flight1,
+                new Connection(departure, connection, departureDateTime.plusHours(2), departureDateTime.plusHours(5))
+        )).when(scheduleIntegrationService).findAllFlights(departure, connection, departureDateTime, arrivalDateTime);
+        Connection flight2 = new Connection(departure, connection, departureDateTime.plusHours(4), departureDateTime.plusHours(5));
+        doReturn(Set.of(
+                new Connection(departure, connection, departureDateTime, departureDateTime.plusHours(2)),
+                flight2
+        )).when(scheduleIntegrationService).findAllFlights(connection, arrival, departureDateTime, arrivalDateTime);
+        Set<Interconnection> flights = service.findFlights(departure, arrival, departureDateTime, arrivalDateTime);
+        assertThat(flights, containsInAnyOrder(
+                new Interconnection(Set.of(directFlight)),
+                new Interconnection(Set.of(flight1, flight2))
+        ));
     }
 
     @Test
     void matchFlights() {
-        String departure = RandomString.make();
-        String connection = RandomString.make();
-        String arrival = RandomString.make();
         LocalDateTime baseDateTime = randomDateTime();
         Connection departureToConnection = new Connection(departure, connection, baseDateTime, baseDateTime.plusHours(3));
         Set<Connection> flightsDepartureToConnection = Set.of(
